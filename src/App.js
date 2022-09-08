57c2bc388365bf81d2fa058a0074aaa3f1b2682e
JenLynX <104281812+JenLynX@users.noreply.github.com>
2022-09-08 11:01:54 -0700

    add weather function, add date function

--- src/App.js
+++ src/App.js
@@ -1,13 +1,46 @@
 import "./App.css";
 
-function App() {
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>React Weather App</p>
-      </header>
+      <div className="appWrap">
+        <form id="city-place">
+          <input
+            className="form-control"
+            type="search"
+            placeholder="Type a City"
+            autofocus="on"
+            autocomplete="off"
+            id="city-enter"
+          />
+
+          <button type="submit">Search</button>
+        </form>
+        <h1>
+          <span className="city">Vancouver</span>
+          <span className="icon">
+            {" "}
+            <img src="" id="emoji" />
+          </span>
+        </h1>
+
+        <div className="row">
+          <div className="col-3">
+            <span className="current-temp">17</span>
+            <span className="unit">°C</span>
+          </div>
+          <div className="col-3">
+            <span className="sky"> Overcast Clouds</span>
+          </div>
+
+          <div className="col-3">
+            <span className="HM">Humidity:30%</span>
+          </div>
+          <div className="col">
+            <span className="WS"> Wind Speed: 10 km</span>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
-
-export default App;
---
+++ src/Date.js
@@ -0,0 +1,5 @@
+import "./App.css";
+
+export default function Date() {
+  return <div className="day"> Friday May 4 2022 6:39pm </div>;
+}
